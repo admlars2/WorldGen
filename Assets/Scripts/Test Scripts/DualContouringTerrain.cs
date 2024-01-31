d343b2ccae4dbd2c5f1060ded473706be6f6e366
OffensiveOOF <atricklars@gmail.com>
2024-01-31 04:08:04 -0800

    octreenode and dualcontouring

---
+++ Assets/Scripts/Test Scripts/DualContouringTerrain.cs
@@ -0,0 +1,114 @@
+using UnityEngine;
+
+public class DualContouringTerrain : MonoBehaviour
+{
+
+    [System.Runtime.InteropServices.StructLayout(System.Runtime.InteropServices.LayoutKind.Sequential)]
+    public struct NodeData {
+        public Vector3 center;
+        public float size;
+        public float density;
+
+        // Constructor
+        public NodeData(Vector3 center, float size, float density) {
+            this.center = center;
+            this.size = size;
+            this.density = density;
+        }
+    }
+
+
+    public ComputeShader dualContouringShader;
+    private ComputeBuffer nodeBuffer;
+
+    // Parameters for the terrain
+    public int initialNodeSize = 50;
+    public int sphereRadius = 50;
+
+    // Root node of the Octree
+    private OctreeNode rootNode;
+
+    void Start()
+    {
+        // Initialize and generate the Octree
+        InitializeOctree();
+        GenerateTerrain();
+    }
+
+    void InitializeOctree()
+    {
+        Vector3 center = new Vector3(0, 0, 0); // Center of the terrain
+        rootNode = new OctreeNode(center, initialNodeSize, sphereRadius, 0);
+        rootNode.RecursiveSubdivide(3);
+    }
+
+    void GenerateTerrain()
+    {
+        OctreeNode[] nodes = rootNode.Flatten().ToArray();
+        int nodeCount = nodes.Length;
+
+        // Assuming 32 bytes per node to be safe
+        int nodeStructSize = 32;
+
+        nodeBuffer = new ComputeBuffer(nodeCount, nodeStructSize);
+
+        // Convert the nodes to the format expected by the compute buffer
+        NodeData[] nodeData = new NodeData[nodeCount];
+        for (int i = 0; i < nodeCount; i++)
+        {
+            nodeData[i] = ConvertToNodeStruct(nodes[i]);
+        }
+
+        nodeBuffer.SetData(nodeData);
+
+        int kernelHandle = dualContouringShader.FindKernel("CSMain");
+        dualContouringShader.SetBuffer(kernelHandle, "NodeBuffer", nodeBuffer);
+
+        // Dispatch the compute shader
+        int threadGroupsX = Mathf.CeilToInt(nodeCount / 8.0f); // Adjust based on your needs
+        dualContouringShader.Dispatch(kernelHandle, threadGroupsX, 1, 1);
+
+        // Retrieve and process mesh data from the shader
+        // ...
+
+        nodeBuffer.Release();
+    }
+
+    void OnDestroy()
+    {
+        if (nodeBuffer != null)
+            nodeBuffer.Release();
+    }
+
+    NodeData ConvertToNodeStruct(OctreeNode node)
+    {
+        return new NodeData(node.Center, node.Size, node.Density);
+    }
+
+
+    void OnDrawGizmos()
+    {
+        DrawNode(rootNode);
+    }
+
+    void DrawNode(OctreeNode node)
+    {
+        if (node == null)
+        {
+            return;
+        }
+
+        // Set Gizmo color and draw a cube
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireCube(node.Center, Vector3.one * node.Size);
+
+        // If the node has children, draw them
+        if (!node.IsLeaf())
+        {
+            foreach (var child in node.Children)
+            {
+                DrawNode(child);
+            }
+        }
+    }
+}
--- Assets/Scripts/Test Scripts/OctreeNode.cs
+++ Assets/Scripts/Test Scripts/OctreeNode.cs
@@ -1,18 +1,97 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
-
-public class OctreeNode : MonoBehaviour
+public class OctreeNode
 {
-    // Start is called before the first frame update
-    void Start()
+    public Vector3 Center {get; private set;}
+    public float Size {get; private set;}
+    public OctreeNode[] Children;
+
+    public float Density {get; set;}
+    public int MaterialType {get; set;}
+
+    private int Radius;
+
+    public OctreeNode(Vector3 center, float size, int radius, float density, int materialType = 0)
     {
-        
+        Center = center;
+        Size = size;
+        Children = new OctreeNode[8];
+
+        Density = density;
+        MaterialType = materialType;
+
+        Radius = radius;
     }
 
-    // Update is called once per frame
-    void Update()
+    public bool IsLeaf()
     {
-        
+        return Children[0] == null;
+    }
+
+    public List<OctreeNode> Flatten()
+    {
+        if (Children[0].IsLeaf()) return new List<OctreeNode>(Children);
+
+        List<OctreeNode> nodes = new List<OctreeNode>();
+
+        foreach(var child in Children)
+        {
+            nodes.AddRange(child.Flatten());   
+        }
+
+        return nodes;
+    }
+
+    public void RecursiveSubdivide(int i)
+    {
+        Subdivide();
+        if (i == 0) return;
+
+        foreach(var child in Children)
+        {
+            child.RecursiveSubdivide(i - 1);
+        }
+    }
+
+    public void SubdivideChildren()
+    {
+        if(IsLeaf()) return;
+
+        foreach(var child in Children)
+        {
+            child.Subdivide();
+        }
+    }
+
+    public void Subdivide()
+    {
+        if (!IsLeaf()) return;
+
+        float childSize = Size / 2;
+        for (int i = 0; i < 8; i++)
+        {
+            float x = Center.x + childSize * (i % 2 == 0 ? -0.5f : 0.5f);
+            float y = Center.y + childSize * (i / 4 == 0 ? -0.5f : 0.5f);
+            float z = Center.z + childSize * (i / 2 % 2 == 0 ? -0.5f : 0.5f);
+
+            Vector3 childCenter = new Vector3(x, y, z);
+
+            float childDensity = CalculateDensity(childCenter, Radius);
+            int materialType = DetermineMaterialType(childCenter);
+
+            Children[i] = new OctreeNode(childCenter, childSize, Radius, childDensity, materialType);
+        }
+    }
+
+    private int DetermineMaterialType(Vector3 childCenter)
+    {
+        return 0;
+    }
+
+    public static float CalculateDensity(Vector3 childCenter, int radius)
+    {
+        return radius*radius - (childCenter.x*childCenter.x + childCenter.y*childCenter.y + childCenter.z * childCenter.z);
     }
 }
